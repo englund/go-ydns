cac055dd650330ea0ce6f41f2d87c204ed44a24f
Victor Englund <victor.englund@gmail.com>
2023-09-18 19:05:43 +0200

    add some cli info and flags

--- cmd/update.go
+++ cmd/update.go
@@ -9,31 +9,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// updateCmd represents the update command
+var host string
+
 var updateCmd = &cobra.Command{
 	Use:   "update",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Update a YDNS record",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("update called")
+		fmt.Println(host)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(updateCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// updateCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// updateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	updateCmd.Flags().StringVarP(&host, "host", "H", "", "The host to update")
+	updateCmd.MarkFlagRequired("host")
 }
